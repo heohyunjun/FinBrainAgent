0a6af201d9fc2f7f2ce7be61131e7a1c12a51de7
heohyunjun <hhj2676@naver.com>
2025-04-01 15:54:41 +0900

    feat : 내부자 거래 조사 팀 그래프

---
+++ team_graph/insider_team_graph.py
@@ -0,0 +1,106 @@
+
+import os 
+from dotenv import load_dotenv
+from datetime import datetime
+from typing import List, Annotated, Literal, Dict, Callable, TypeVar, Tuple, Type, Generic, Optional, Union, Any
+
+from langchain.prompts import PromptTemplate
+from langgraph.graph import StateGraph, MessagesState, START, END
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
+from langgraph.types import Command
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
+
+from agents.agent_library import agent_configs
+
+from typing_extensions import TypedDict 
+load_dotenv() 
+
+
+
+Model_NAME = "gpt-4o-mini-2024-07-18"
+
+insider_team_leader_llm = ChatOpenAI(model=Model_NAME)
+
+
+class InsiderState(MessagesState):
+    query: str
+
+def data_retrieval_leader_node(state: InsiderState):
+    return state
+
+insider_team_members = agent_configs['insider_team_leader']['members']
+insider_team_leader_system_prompt = agent_configs['insider_team_leader']['prompt']
+insider_team_options_for_next = insider_team_members + ['FINISH']
+
+insider_team_leader_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", insider_team_leader_system_prompt),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "Given the conversation above, who should act next? "
+            "Or should we FINISH? Select one of: {options}",
+        ),
+    ]
+).partial(options=str(insider_team_options_for_next), members=insider_team_members)
+
+class InsiderTeamRouter(TypedDict):
+    """Worker to route to next. If no workers needed, route to FINISH."""
+    next: Literal[*insider_team_options_for_next]
+
+
+def insider_team_leader_node(state: InsiderState) -> Command[Literal[*insider_team_members, "data_retrieval_leader"]]:
+    insider_team_chain = insider_team_leader_prompt | insider_team_leader_llm.with_structured_output(InsiderTeamRouter)
+    response = insider_team_chain.invoke(state)
+
+    goto = response["next"]
+    if goto == "FINISH":
+        goto = "data_retrieval_leader"
+
+    return Command(goto=goto)
+
+
+
+domestic_insider_researcher_agent = create_react_agent(
+    insider_team_leader_llm, 
+    tools=agent_configs['domestic_insider_researcher']['tools'],
+    prompt=agent_configs['domestic_insider_researcher']['prompt']
+)
+
+international_insider_researcher_agent = create_react_agent(
+    insider_team_leader_llm, 
+    tools=agent_configs['international_insider_researcher']['tools'], 
+    prompt=agent_configs['international_insider_researcher']['prompt']
+)
+
+def domestic_insider_researcher_node(state: InsiderState) -> Command[Literal["insider_team_leader"]]:
+    result = domestic_insider_researcher_agent.invoke(state)
+    return Command(
+        update={'messages': [HumanMessage(content=result['messages'][-1].content, name='domestic_insider_researcher')]},
+        goto='insider_team_leader'
+    )
+
+
+
+
+def international_insider_researcher_node(state: InsiderState) -> Command[Literal["insider_team_leader"]]:
+    result = international_insider_researcher_agent.invoke(state)
+
+    return Command(
+        update={'messages': [HumanMessage(content=result['messages'][-1].content, name='international_insider_researcher')]},
+        goto='insider_team_leader'
+    )
+
+
+insider_graph_builder = StateGraph(InsiderState)
+insider_graph_builder.add_node("insider_team_leader", insider_team_leader_node)
+insider_graph_builder.add_node("data_retrieval_leader", data_retrieval_leader_node)
+insider_graph_builder.add_node("domestic_insider_researcher", domestic_insider_researcher_node)
+insider_graph_builder.add_node("international_insider_researcher", international_insider_researcher_node)
+
+insider_graph_builder.add_edge(START, "insider_team_leader")
+insider_graph = insider_graph_builder.compile()
