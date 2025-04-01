11e1d379737a788129464e55d87f27074d5a1061
heohyunjun <hhj2676@naver.com>
2025-04-01 16:46:32 +0900

    chore: datateam ipynb 디버깅 코드 .py로 변환

--- data_team_subgraph.py
+++ data_team_subgraph.py
@@ -1,62 +1,106 @@
-
-import json
-import streamlit as st
-import openai
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
-from typing import List, Annotated, Literal, Dict, Callable, TypeVar, Tuple, Type, Generic, Optional, Union, Any
+from typing import Literal, Optional
 from typing_extensions import TypedDict 
 
 # LangChain 및 LangGraph 관련 라이브러리
-from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.output_parsers import StrOutputParser
+
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.language_models.chat_models import BaseChatModel
-from langchain.agents import AgentExecutor
 
 from langgraph.types import Command
-from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import create_react_agent
 from langsmith import utils 
-from datetime import datetime, timedelta
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
 
-
-from sec_tool.insider_trade_tool import SECInsiderTradeAPI
-from sec_tool.market_data_tool import MarketDataTools, FinancialDataTools, EconomicDataTools
-
+from agents.agent_library import agent_configs
 from team_graph.report_team_subgraph import report_graph
 from team_graph.general_chat_subgraph import general_graph
-
+from team_graph.insider_team_graph import insider_graph 
 
 # 환경 변수 로드
 load_dotenv()
 print(utils.tracing_is_enabled())
 
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
 class AgentState(MessagesState):
     query: str
 
-
-
 llm = ChatOpenAI(model="gpt-4o-mini-2024-07-18")
-
-
-news_and_sentiment_retrieval_prompt = f"""You are an expert in finding financial news and analyst opinions. 
-Provide fact only not opinions
-The current time is {current_time}. Use this time when invoking tools that require the current time as an argument."""
 
 # 시장 조사 에이전트
 news_and_sentiment_retrieval_agent = create_react_agent(
     llm, 
-    tools=[MarketDataTools.get_stock_news, MarketDataTools.get_websearch_tool()],
-    prompt=news_and_sentiment_retrieval_prompt
+    tools=agent_configs['news_and_sentiment_retrieval_agent']['tools'],
+    prompt=agent_configs['news_and_sentiment_retrieval_agent']['prompt']
 )
+
+market_data_retrieval_agent = create_react_agent(
+    llm, 
+    tools=agent_configs['market_data_retrieval_agent']['tools'], 
+    prompt = agent_configs['market_data_retrieval_agent']['prompt']
+)
+
+
+economic_data_retrieval_agent = create_react_agent(
+    llm, 
+    tools=agent_configs['economic_data_retrieval_agent']['tools'], 
+    prompt=agent_configs['economic_data_retrieval_agent']['prompt']
+)
+
+
+financial_statement_retrieval_agent = create_react_agent(
+    llm,
+    tools=agent_configs['financial_statement_retrieval_agent']['tools'],
+    prompt = agent_configs['financial_statement_retrieval_agent']['prompt']
+)
+
+
+data_retrieval_team_members=agent_configs['data_retrieval_leader_agent']['members']
+data_retrieval_options_for_next = data_retrieval_team_members + ["FINISH"]
+data_retrieval_leader_system_prompt = agent_configs['data_retrieval_leader_agent']['prompt']
+data_retrieval_leader_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", data_retrieval_leader_system_prompt),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "Given the conversation above, who should act next? "
+            "Or should we FINISH? Select one of: {options}. "
+            "Return a JSON with 'next', 'is_vague', and optionally 'response' if vague. "
+            "If not vague and no clarification is needed, leave 'response' empty."
+        ),
+    ]
+).partial(options=str(data_retrieval_options_for_next), members=", ".join(data_retrieval_team_members))
+
+
+data_cleansing_system_prompt = agent_configs['data_cleansing_agent']['prompt']
+data_cleansing_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", data_cleansing_system_prompt),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "Given the conversation above, "
+            "Refine and process this data to align with the user's original question : {query}"
+        )
+    ]
+)
+
+supervisor_members = agent_configs['supervisor']['members']
+supervisor_options_for_next = supervisor_members + ["FINISH"]
+supervisor_system_prompt = agent_configs['supervisor']['prompt']
+supervisor_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", supervisor_system_prompt),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "Given the conversation above, who should act next? "
+            "Or should we FINISH? Select one of: {options}",
+        ),
+    ]
+).partial(options=str(supervisor_options_for_next), members=", ".join(supervisor_members))
 
 def news_and_sentiment_retrieval_node(state: AgentState) -> Command[Literal["data_retrieval_leader"]]:
     """
@@ -77,15 +121,6 @@ def news_and_sentiment_retrieval_node(state: AgentState) -> Command[Literal["dat
         goto='data_retrieval_leader'
     )
 
-
-
-
-stock_research_tools = [MarketDataTools.get_stock_price]
-stock_research_agent = create_react_agent(
-    llm, tools=stock_research_tools, state_modifier='You are an expert in  market data retrieval. Your mission is to collect stock price information. Provide fact only not opinions'
-)
-
-
 def market_data_retrieval_node(state: AgentState) -> Command[Literal["data_retrieval_leader"]]:
     """
     실시간 주가 및 거래량 수집
@@ -95,23 +130,12 @@ def market_data_retrieval_node(state: AgentState) -> Command[Literal["data_retri
     Returns:
         Command: supervisor node로 이동하기 위한 명령을 반환
     """
-    result = stock_research_agent.invoke(state)
+    result = market_data_retrieval_agent.invoke(state)
 
     return Command(
         update={'messages': [HumanMessage(content=result['messages'][-1].content, name='market_data_retrieval')]},
         goto='data_retrieval_leader'
     )
-
-
-economic_data_prompt = f"""You are an expert in macroeconomic data retrieval.
-Your mission is to collect accurate and up-to-date macroeconomic indicators from reliable sources.
-You have access to the following tools: [get_core_cpi_data, get_core_pce_data, get_personal_income_data,get_mortgage_rate_data, get_unemployment_rate_data, get_jobless_claims_data.]
-The current time is {current_time}. Use this time when invoking tools that require the current time as an argument.
-Provide factual data only, without interpretation or opinion."""
-
-economic_data_retrieval_tools = [EconomicDataTools.get_core_cpi_data]
-economic_data_retrieval_agent = create_react_agent(
-    llm, tools=economic_data_retrieval_tools, prompt=economic_data_prompt)
 
 def economic_data_retrieval_node(state: AgentState) -> Command[Literal["data_retrieval_leader"]]:
     """
@@ -130,16 +154,6 @@ def economic_data_retrieval_node(state: AgentState) -> Command[Literal["data_ret
     )
 
 
-# 도구 리스트
-financial_statement_retrieval_tools = [FinancialDataTools.get_income_statement, FinancialDataTools.get_financial_event_filings]
-
-# 단일 에이전트 정의
-financial_statement_retrieval_agent = create_react_agent(
-    llm,
-    tools=financial_statement_retrieval_tools,
-    state_modifier='You are an expert in collecting corporate financial statements and performance data. Provide facts only, no opinions.'
-)
-
 def financial_statement_retrieval_node(state: AgentState) -> Command[Literal["data_retrieval_leader"]]:
     """
     회사 재무 보고서와 주요 사건 관련 자료를 조사하는 노드.
@@ -156,77 +170,6 @@ def financial_statement_retrieval_node(state: AgentState) -> Command[Literal["da
         update={'messages': [HumanMessage(content=result['messages'][-1].content, name='financial_statement_retrieval')]},
         goto='data_retrieval_leader'
     )
-
-
-
-insider_tracker_system_prompt = f"""You are an insider trading analyst.
-You must provide factual data only, without any personal opinions or speculations.
-The current time is {current_time}. Use this time when invoking tools that require the current time as an argument."""
-
-
-insider_tracker_research_tools = [SECInsiderTradeAPI.fetch_filings]
-insider_tracker_research_agent = create_react_agent(
-    llm, tools=insider_tracker_research_tools, prompt=insider_tracker_system_prompt
-)
-
-
-
-def insider_tracker_research_node(state: AgentState) -> Command[Literal["data_retrieval_leader"]]:
-    """
-    내부자 거래 내역 조사 node 
-    Args:
-        state (MessagesState): 현재 메시지 상태를 나타내는 객체
-
-    Returns:
-        Command: supervisor node로 이동하기 위한 명령을 반환
-    """
-    result = insider_tracker_research_agent.invoke(state)
-
-    return Command(
-        update={'messages': [HumanMessage(content=result['messages'][-1].content, name='insider_tracker_research')]},
-        goto='data_retrieval_leader'
-    )
-
-
-
-
-data_retrieval_team_members = [
-    "news_and_sentiment_retrieval", "market_data_retrieval", 
-    "financial_statement_retrieval","insider_tracker_research",
-    "economic_data_retrieval"
-    ]
-data_retrieval_options_for_next = data_retrieval_team_members + ["FINISH"]
-
-data_retrieval_leader_system_prompt = (
-    "You are a supervisor tasked with managing a conversation between the following specialized workers: {members}.\n"
-    "Each worker handles specific tasks:\n"
-    "- news_and_sentiment_retrieval: Handles news articles, market trends, general industry updates.\n"
-    "- market_data_retrieval: Handles current stock prices\n"
-    "- financial_statement_retrieval: Handles income statements, financial statements, and SEC filings.\n"
-    "- insider_tracker_research: Handles insider trading filings and insider transactions.\n"
-    "- economic_data_retrieval: Handles macroeconomic data\n"
-    "Given the user request, strictly select ONLY ONE most suitable worker to act next based on the task description above.\n"
-    "If the request is too vague and lacks specific details, set 'is_vague' to true and 'next' to 'FINISH', "
-    "and optionally provide a clarification request in 'response'. "
-    "When finished normally, set 'is_vague' to false and 'next' to 'FINISH'. "
-    "Do not fill 'response' unless explicitly needed for clarification."
-)
-
-
-# ChatPromptTemplate 생성
-data_retrieval_leader_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", data_retrieval_leader_system_prompt),
-        MessagesPlaceholder(variable_name="messages"),
-        (
-            "system",
-            "Given the conversation above, who should act next? "
-            "Or should we FINISH? Select one of: {options}. "
-            "Return a JSON with 'next', 'is_vague', and optionally 'response' if vague. "
-            "If not vague and no clarification is needed, leave 'response' empty."
-        ),
-    ]
-).partial(options=str(data_retrieval_options_for_next), members=", ".join(data_retrieval_team_members))
 
 class DataTeamRouter(TypedDict):
     """Worker to route to next. If no workers needed or question is vague, route to FINISH."""
@@ -264,31 +207,6 @@ def data_retrieval_leader_node(state: AgentState) -> Command[Literal[*data_retri
 
     return Command(goto=goto)
 
-
-
-data_cleansing_system_prompt = (
-    "You are a data cleansing agent responsible for refining raw data collected by the data team. "
-    "Your role is to process the collected data to ensure it directly addresses the user's original question. "
-    "Your tasks are: "
-    "- Remove irrelevant or redundant information that does not help answer the user's question. "
-    "- Fix inconsistencies (e.g., missing values, incorrect formats) to make the data usable. "
-    "- Structure the data in a concise, clear format tailored to the user's request. "
-    "Provide only factual, cleaned data without opinions or speculations. ")
-
-# ChatPromptTemplate 생성
-data_cleansing_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", data_cleansing_system_prompt),
-        MessagesPlaceholder(variable_name="messages"),
-        (
-            "system",
-            "Given the conversation above, "
-            "Refine and process this data to align with the user's original question : {query}"
-        )
-    ]
-)
-
-
 def data_cleansing_node(state: AgentState) -> Command[Literal["supervisor"]]:
     """
     데이터 클렌징 노드. 수집된 데이터를 정제하여 supervisor로 전달
@@ -314,41 +232,9 @@ def data_cleansing_node(state: AgentState) -> Command[Literal["supervisor"]]:
         goto='supervisor'
     )
 
-supervisor_members = ["data_retrieval_leader", "general_team_leader"]
-supervisor_options_for_next = supervisor_members + ["FINISH"]
-
-supervisor_system_prompt = (
-    "You are a supervisor tasked with overseeing a conversation in an AI agent service designed to provide financial and investment advice. "
-    "Your role is to act as the central coordinator, directing user requests to the appropriate specialized workers: {members}.\n"
-    "Each worker handles specific tasks:\n"
-    "- 'data_team_leader':  role is responsible for collecting and refining the data required to answer user questions.\n"
-    "- 'general_team_leader': role is responsible for handling general knowledge questions outside finance or investing.\n"
-    "Given the user request, strictly select ONLY ONE most suitable worker to act next based on the task description above. "
-    "When finished, respond with FINISH."
-)
-
-
-# ChatPromptTemplate 생성
-supervisor_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", supervisor_system_prompt),
-        MessagesPlaceholder(variable_name="messages"),
-        (
-            "system",
-            "Given the conversation above, who should act next? "
-            "Or should we FINISH? Select one of: {options}",
-        ),
-    ]
-).partial(options=str(supervisor_options_for_next), members=", ".join(supervisor_members))
-
-
 class Router(TypedDict):
-    """Worker to route to next. If no workers needed, route to FINISH."""
-
     next: Literal[*supervisor_options_for_next]
     
-
-
 
 def supervisor_node(state: AgentState) -> Command[Literal[*supervisor_members, "reporter"]]:
     """
@@ -380,16 +266,16 @@ graph_builder.add_node("supervisor", supervisor_node)
 graph_builder.add_node("news_and_sentiment_retrieval", news_and_sentiment_retrieval_node)
 graph_builder.add_node("market_data_retrieval", market_data_retrieval_node)
 graph_builder.add_node("financial_statement_retrieval", financial_statement_retrieval_node)
-graph_builder.add_node("insider_tracker_research", insider_tracker_research_node)
 graph_builder.add_node("data_retrieval_leader", data_retrieval_leader_node)
 graph_builder.add_node("data_cleansing", data_cleansing_node)
 graph_builder.add_node("economic_data_retrieval", economic_data_retrieval_node)
+graph_builder.add_node("insider_team_leader", insider_graph)
 graph_builder.add_node("reporter", report_graph)
 graph_builder.add_node("general_team_leader", general_graph)
 
 graph_builder.add_edge(START, "supervisor")
+graph_builder.add_edge("insider_team_leader", "data_retrieval_leader")
 graph_builder.add_edge("reporter", END)
 graph_builder.add_edge("general_team_leader", END)
 
 graph = graph_builder.compile()
-
